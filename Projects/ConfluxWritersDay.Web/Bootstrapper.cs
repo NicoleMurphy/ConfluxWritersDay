2d37483bf920b8f400dcea0795e565f217ef75a8
Tim Murphy <tim@26tp.com>
2013-07-28 22:09:31 +1000

    Configured Nancy to serve /Scripts folder.

--- Projects/ConfluxWritersDay.Web/Bootstrapper.cs
+++ Projects/ConfluxWritersDay.Web/Bootstrapper.cs
@@ -1,11 +1,15 @@
-﻿namespace ConfluxWritersDay.Web
-{
-    using Nancy;
+﻿using Nancy;
+using Nancy.Conventions;
 
+namespace ConfluxWritersDay.Web
+{
     public class Bootstrapper : DefaultNancyBootstrapper
     {
-        // The bootstrapper enables you to reconfigure the composition of the framework,
-        // by overriding the various methods and properties.
-        // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Scripts"));
+        }
     }
 }
