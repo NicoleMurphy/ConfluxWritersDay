f95c3d0c5a569f36ea5b64cda1f86357bb26f012
Tim Murphy <tim@26tp.com>
2013-09-01 03:17:57 +1000

    Add stubs for Registration specs.

---
+++ Projects/ConfluxWritersDay.Tests/Specifications/RegistrationSpecifications.cs
@@ -0,0 +1,165 @@
+﻿using BddMagic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestMagic;
+
+namespace ConfluxWritersDay.Tests.Specifications
+{
+    [TestClass]
+    public class RegistrationSpecifications : BddFeature
+    {
+        public RegistrationSpecifications()
+            : base("Registration Page", "todo: story")
+        {
+        }
+
+        [TestMethod]
+        public void RegistrationDetails()
+        {
+            var s = this.Scenario("Registration Details");
+
+            s["Given I am on the registration page"] = null;
+            s["And my first name is entered"] = null;
+            s["And my last name is entered"] = null;
+            s["And my address is entered"] = null;
+            s["And my phone is entered"] = null;
+            s["And my email address is entered"] = null;
+            s["And entered special dietary requirements"] = null;
+            s["And selected payment method of Cheque, PayPal or Direct Deposit"] = null;
+            s["And selected a member organisation of Conflux 9 Member, CSFG Member, ACT Writers Centre Member or Non-Member"] = null;
+            s["When I submit my registration"] = null;
+            s["Then I will see thank you page"] = null;
+            s["And I will receive an email"] = null;
+
+            s.Execute();
+        }
+
+        [TestMethod]
+        public void FirstNameIsRequired()
+        {
+            var s = this.Scenario("First name is required");
+
+            s["Given I am on the registration page"] = null;
+            s["And I have not entered my first name"] = null;
+            s["When I submit my registration"] = null;
+            s["Then I will see warning message that first name is required"] = null;
+
+            s.Execute();
+        }
+
+        [TestMethod]
+        public void LastNameIsRequired()
+        {
+            var s = this.Scenario("Last name is required");
+
+            s["Given I am on the registration page"] = null;
+            s["And I have not entered my email address"] = null;
+            s["When I submit my registration"] = null;
+            s["Then I will see warning message that email address is required"] = null;
+
+            s.Execute();
+        }
+
+        [TestMethod]
+        public void EmailAddressIsRequired()
+        {
+            var s = this.Scenario("Email address is required");
+
+            s["Given I am on the registration page"] = null;
+            s["And I have not entered my first name"] = null;
+            s["When I submit my registration"] = null;
+            s["Then I will see warning message that first name is required"] = null;
+
+            s.Execute();
+        }
+
+        [TestMethod]
+        public void MembershipOrganisation()
+        {
+            var s = this.Scenario("Membership organisation");
+
+            s["Given I am on the registration page"] = null;
+            s["When I am a member"] = null;
+            s["Then the price is '$120.00'"] = null;
+
+            s.Execute();
+        }
+
+        [TestMethod]
+        public void NoMembershipOrganisation()
+        {
+            var s = this.Scenario("Member organisation");
+            
+            s["Given I am on the registration page"] = null;
+            s["When I am not a member"] = null;
+            s["Then the price is '$150.00'"] = null;
+
+            s.Execute();
+        }
+
+        [TestMethod]
+        public void First30Registrations()
+        {
+            var s = this.Scenario("First 30 registration");
+            
+            s["Given I am on the registration page"] = null;
+            s["When I am one of the first 30 registrations"] = null;
+            s["Then the price is '$90.00'"] = null;
+
+            s.Execute();
+        }
+
+        [TestMethod]
+        public void _31PlusRegistration()
+        {
+            var s = this.Scenario("31+ registration");
+            
+            s["Given I am on the registration page"] = null;
+            s["And I was 30th to start registration"] = null;
+            s["And someone else typed quicker than me"] = null;
+            s["When I hit submit"] = null;
+            s["Then I am told I missed out"] = null;
+
+            s.Execute();
+        }
+
+        [TestMethod]
+        public void PaymentByDirectDeposit()
+        {
+            var s = this.Scenario("direct deposit");
+            
+            s["Given I have completed registration "] = null;
+            s["And the payment method is direct deposit payment"] = null;
+            s["When I am sent an email"] = null;
+            s["Then I the direct deposit invoice is attached"] = null;
+
+            s.Execute();
+        }
+
+        [TestMethod]
+        public void PaymentByPayPal()
+        {
+            var s = this.Scenario("paypal");
+
+            s["Given I have completed registration "] = null;
+            s["And the payment method is Paypal payment"] = null;
+            s["When I am sent an email"] = null;
+            s["Then I the paypal invoice is attached"] = null;
+
+            s.Execute();
+        }
+
+        [TestMethod]
+        public void PaymentByCheque()
+        {
+            var s = this.Scenario("cheque");
+
+            s["Given I have completed registration "] = null;
+            s["And the payment method is cheque payment"] = null;
+            s["When I am sent an email"] = null;
+            s["Then I the cheque invoice is attached"] = null;
+
+            s.Execute();
+        }
+    }
+}
