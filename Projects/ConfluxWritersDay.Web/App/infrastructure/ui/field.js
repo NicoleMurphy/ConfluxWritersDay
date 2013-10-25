30708025f531a791cc5a980d6504cec32373b8d5
Tim Murphy <tim@26tp.com>
2013-10-26 07:51:35 +1100

    Code formatting.

--- Projects/ConfluxWritersDay.Web/App/infrastructure/ui/field.js
+++ Projects/ConfluxWritersDay.Web/App/infrastructure/ui/field.js
@@ -66,30 +66,35 @@ textfield.directive('field', function ($compile, $http, $templateCache, $interpo
 
             tryRemoveAttribute(element, 'label');
 
-            // Load up the template for this kind of field
-            var template = attrs.template || 'field';   // Default to the simple input if none given
-            var getFieldElement = $http.get('/App/infrastructure/ui/' + template + '.template.html', { cache: $templateCache }).then(function (response) {
-                var newElement = angular.element(response.data);
-                var inputElement = findInputElement(newElement);
+            var template = attrs.template || 'field';
 
-                // Copy over the attributes to the input element
-                // At least the ng-model attribute must be copied because we can't use interpolation in the template
-                angular.forEach(element[0].attributes, function (attribute) {
-                    var value = attribute.value;
-                    var key = attribute.name;
-                    inputElement.attr(key, value);
+            var getFieldElement = $http.get('/App/infrastructure/ui/' + template + '.template.html', { cache: $templateCache })
+
+                .then(function (response) {
+
+                    var newElement = angular.element(response.data);
+                    var inputElement = findInputElement(newElement);
+
+                    // Copy over the attributes to the input element
+                    // At least the ng-model attribute must be copied because we can't use interpolation in the template
+                    angular.forEach(element[0].attributes, function (attribute) {
+                        var value = attribute.value;
+                        var key = attribute.name;
+                        inputElement.attr(key, value);
+                    });
+
+                    // Update the label's contents
+                    var labelElement = newElement.find('label');
+                    labelElement.html(labelContent);
+
+                    return newElement;
                 });
 
-                // Update the label's contents
-                var labelElement = newElement.find('label');
-                labelElement.html(labelContent);
-
-                return newElement;
-            });
-
             return function (scope, element, attrs, formController) {
+
                 // We have to wait for the field element template to be loaded
                 getFieldElement.then(function (newElement) {
+
                     // Our template will have its own child scope
                     var childScope = scope.$new();
 
@@ -103,26 +108,35 @@ textfield.directive('field', function ($compile, $http, $templateCache, $interpo
                     // (We need to set the input element's name here before we compile.
                     // If we leave it to interpolation, the formController doesn't pick it up)
                     var inputElement = findInputElement(newElement);
+
                     inputElement.attr('name', childScope.$modelId);
                     inputElement.attr('id', childScope.$modelId);
+
                     newElement.find('label').attr('for', childScope.$modelId);
 
                     // TODO: Consider moving this validator stuff into its own directive
                     // and use a directive controller to wire it all up
                     childScope.$validationMessages = {};
+
                     angular.forEach(validationMessages, function (validationMessage) {
-                        // We need to watch incase it has interpolated values that need processing
-                        scope.$watch(validationMessage.getMessage, function (message) {
-                            childScope.$validationMessages[validationMessage.key] = message;
-                        });
+
+                        // We need to watch in case it has interpolated values that need processing
+                        scope.$watch(
+                            validationMessage.getMessage,
+                            function (message) {
+                                childScope.$validationMessages[validationMessage.key] = message;
+                            }
+                        );
                     });
 
                     // We must compile our new element in the postLink function rather than in the compile function
                     // (i.e. after any parent form element has been linked)
                     // otherwise the new input won't pick up the FormController
                     $compile(newElement)(childScope, function (clone) {
+
                         // Place our new element after the original element
                         element.after(clone);
+
                         // Remove our original element
                         element.remove();
                     });
@@ -130,6 +144,7 @@ textfield.directive('field', function ($compile, $http, $templateCache, $interpo
                     // Only after the new element has been compiled do we have access to the ngModelController
                     // (i.e. formController[childScope.name])
                     if (formController) {
+
                         childScope.$form = formController;
                         childScope.$field = formController[childScope.$modelId];
                     }
