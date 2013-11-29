9d56f5f95213f49f16c5cb9271d493c18b045397
Tim Murphy <tim@26tp.com>
2013-11-30 06:49:05 +1100

    Installed SpecFlow & registration feature.

---
+++ Projects/ConfluxWritersDay.Specifications/Steps/RegistrationSteps.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace ConfluxWritersDay.Specifications.Steps
+{
+    [Binding]
+    public class RegistrationSteps
+    {
+        [Given(@"I am choosing to register online")]
+public void GivenIAmChoosingToRegisterOnline()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"I am on the registration page")]
+public void GivenIAmOnTheRegistrationPage()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"my first name is entered")]
+public void GivenMyFirstNameIsEntered()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"my last name is entered")]
+public void GivenMyLastNameIsEntered()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"my address is entered")]
+public void GivenMyAddressIsEntered()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"my phone is entered")]
+public void GivenMyPhoneIsEntered()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"my email address is entered")]
+public void GivenMyEmailAddressIsEntered()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"entered special dietary requirements")]
+public void GivenEnteredSpecialDietaryRequirements()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"selected payment method of Cheque, PayPal or Direct Deposit")]
+public void GivenSelectedPaymentMethodOfChequePayPalOrDirectDeposit()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"selected a member organisation of Conflux (.*) Member, CSFG Member, ACT Writers Centre Member or Non-Member")]
+public void GivenSelectedAMemberOrganisationOfConfluxMemberCSFGMemberACTWritersCentreMemberOrNon_Member(int p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"I have not entered my first name")]
+public void GivenIHaveNotEnteredMyFirstName()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"I have not entered my email address")]
+public void GivenIHaveNotEnteredMyEmailAddress()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"I was (.*)th to start registration")]
+public void GivenIWasThToStartRegistration(int p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"someone else typed quicker than me")]
+public void GivenSomeoneElseTypedQuickerThanMe()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"I have completed registration")]
+public void GivenIHaveCompletedRegistration()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"the payment method is direct deposit payment")]
+public void GivenThePaymentMethodIsDirectDepositPayment()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"the payment method is Paypal payment")]
+public void GivenThePaymentMethodIsPaypalPayment()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Given(@"the payment method is cheque payment")]
+public void GivenThePaymentMethodIsChequePayment()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I navigate to this page")]
+public void WhenINavigateToThisPage()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I submit my registration")]
+public void WhenISubmitMyRegistration()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I am a member")]
+public void WhenIAmAMember()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I am not a member")]
+public void WhenIAmNotAMember()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I am one of the first (.*) registrations")]
+public void WhenIAmOneOfTheFirstRegistrations(int p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I hit submit")]
+public void WhenIHitSubmit()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [When(@"I am sent an email")]
+public void WhenIAmSentAnEmail()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I want the process to be quick and easy")]
+public void ThenIWantTheProcessToBeQuickAndEasy()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I want it clearly explained")]
+public void ThenIWantItClearlyExplained()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I want multiple options for payment")]
+public void ThenIWantMultipleOptionsForPayment()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I want an automatic email confirming that my registration has gone through")]
+public void ThenIWantAnAutomaticEmailConfirmingThatMyRegistrationHasGoneThrough()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I will see thank you page")]
+public void ThenIWillSeeThankYouPage()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I will receive an email")]
+public void ThenIWillReceiveAnEmail()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I will see warning message that first name is required")]
+public void ThenIWillSeeWarningMessageThatFirstNameIsRequired()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I will see warning message that email address is required")]
+public void ThenIWillSeeWarningMessageThatEmailAddressIsRequired()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"the price is '(.*)'")]
+public void ThenThePriceIs(string p0)
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I am told I missed out")]
+public void ThenIAmToldIMissedOut()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I the direct deposit invoice is attached")]
+public void ThenITheDirectDepositInvoiceIsAttached()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I the paypal invoice is attached")]
+public void ThenIThePaypalInvoiceIsAttached()
+{
+    ScenarioContext.Current.Pending();
+}
+
+        [Then(@"I the cheque invoice is attached")]
+public void ThenITheChequeInvoiceIsAttached()
+{
+    ScenarioContext.Current.Pending();
+}
+    }
+}
